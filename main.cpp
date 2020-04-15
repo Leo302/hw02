93650812519a66af401e259751b2c13b856a5fa3
Leo302 <lkiuy00@gamil.com>
2020-04-15 22:01:59 +0800

    Add hw02

--- main.cpp
+++ main.cpp
@@ -6,14 +6,11 @@ AnalogIn Ain(A0);
 DigitalIn  Switch(SW3);
 DigitalOut redLED(LED1);
 DigitalOut greenLED(LED2);
-
 BusOut display(D6, D7, D9, D10, D11, D5, D4, D8);
 char table[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
-
 int sample = 1000;
 int i;
-
-float ADCdata[1000];
+float data[1000];
 float j;
 
 int main(){
@@ -21,11 +18,11 @@ int main(){
     redLED = 0;
     greenLED = 1;
     for (i = 0; i < sample; i++){
-        ADCdata[i] = Ain;
+        data[i] = Ain;
         wait(1./sample);
         if(i>=1){
-            if(ADCdata[i-1] <= 0.2){
-                if(ADCdata[i] > 0.2){
+            if(data[i] <= 0.1){
+                if(data[i+1] > 0.1){
                     freq += 1 ;                    
                 }
             }            
@@ -33,29 +30,30 @@ int main(){
     }
 
     for (i = 0; i < sample; i++){
-        pc.printf("%1.3f\r\n", ADCdata[i]);
+        pc.printf("%1.3f\r\n", data[i]);
         wait(0.1);
     }
-
-    freq = freq*97/100;    
-    while(1){
+    
+    int final;
+    while(true){
         if( Switch == 0 ){
-            redLED = 0;
-            greenLED = 1;
+            redLED = 1;
+            greenLED = 0;
             display = table[(freq / 100) % 10];
             wait(1.0);
             display = table[(freq / 10) % 10];
             wait(1.0);
-            display = int(table[freq % 10])+0x80;
+            final = table[freq % 10];
+            display = final + 0x80;
             wait(1.0);
         }
-        if( Switch == 1 ){
+        else{
             display  = 0x00;
-            redLED = 1;
-            greenLED = 0;
+            redLED = 0;
+            greenLED = 1;
         }
         for( j=0; j<2; j+=0.05 ){
-            Aout = 0.5 + 0.5*sin(j*3.14159);
+            Aout = 0.5 + 0.5*sin(j*3.14);
             wait(1./freq/40);
         }
     }
